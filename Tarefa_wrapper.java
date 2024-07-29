0be9491c758d55133803324eced2ea9cf6a1e8d0
JVZYZ <108882290+JVZYZ@users.noreply.github.com>
2024-07-29 16:16:47 -0300

    Add da tarefa.java - módulo 9 - backend

---
+++ Tarefa_wrapper.java
@@ -0,0 +1,33 @@
+package tarefa;
+
+import java.util.Scanner;
+
+public class Tarefa_wrapper {
+	public static void main(String[] args) {
+
+		
+		        
+		       Scanner scanner = new Scanner(System.in);
+
+		        
+		       System.out.println("Digite um número");
+
+		        
+		       int numero = scanner.nextInt();
+
+		        
+		       System.out.println(numero);
+
+		        
+		       Double numero2 = (double) numero;
+
+		        
+		       System.out.println(numero2); 
+		 }
+	}
+		    
+		
+
+	
+	     
+	     
